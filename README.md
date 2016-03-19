3bdaff661338bff0a22b22db444283d235cb0806
Tatsuya Nanjo <noralife@gmail.com>
2016-03-19 22:05:39 +0900

    Initial Release

---
+++ README.md
@@ -0,0 +1,11 @@
+node-red-contrib-hello-world
+========================
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to send Hello World.
+
+Install
+-------
+
+Run the following command in your Node-RED user directory - typically `~/.node-red`
+
+    npm install node-red-contrib-hello-world
+
---
+++ hello-world/hello-world.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function HelloWorldNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+	    msg.payload = "Hello World"
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("hello-world", HelloWorldNode);
+}
